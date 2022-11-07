a1eddf5835b3594e8f39d5e7017aef52d5a03a1b
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-11-07 04:23:20 -0800

    Delete oparation complited

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const users = useLoaderData();
+    const [displayUsers, setDisplayUsers] = useState(users)
+
+    const handleDelete = user => {
+
+        const agree = window.confirm(`Are you sure you want to delete: ${user.name}`);
+        if(agree){
+            console.log('Delling id is: ', user._id);
+            fetch(`http://localhost:5000/users/${user._id}`, {
+                method: 'DELETE'
+
+            })
+            .then(res =>res.json())
+            .then(data => {
+                
+                console.log(data);
+                if(data.deletedCount > 0){
+                    alert('user deleter successfully');
+                    const remainingUSers = displayUsers.filter(usr =>usr._id !== user._id);
+                    setDisplayUsers(remainingUSers);
+                }
+            
+            });
+
+        }
+        
+    }
+
+
     return (
         <div>
-            <h1>This is home</h1>
+            <h1>This is home:{displayUsers.length}</h1>
+            {
+                displayUsers.map(user => <p kay={user._id}>{user.name},{user.email}<button onClick={() => handleDelete(user)}>X</button></p>)
+            }
         </div>
     );
 };
